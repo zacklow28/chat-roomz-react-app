c3c6681800f0371117367d8d6fa8a1a7ec132ef2
zacklow28 <zo.ngyun@hotmail.com>
2025-03-30 20:30:35 +0800

    Set up simple connection

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
+import io from 'socket.io-client';
+
+let socket;
 
 const Chat = () => {
+    const location = useLocation();
+    const ENDPOINT = "http://localhost:5000";
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+        setRoom(room);
+        setName(name);
+
+        //only connect if socket doesn't exist
+        if (!socket) {
+            socket = io(ENDPOINT);
+            socket.emit("join", { name, room }, ({ }) => {
+            });
+        };
+
+        //cleanup socket connection when component unmount
+        return () => {
+            socket.disconnect();
+        }
+
+    }, [location.search]);
+
     return (
-        <h1>Chat</h1>
+        <h1>{`Hello ${name}! Welcome to Room ${room}`}</h1>
     );
 };
 
